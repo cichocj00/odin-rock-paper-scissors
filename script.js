6fbcedb83c127789025e34ff3f2dfcf7f842ee8c
cichocj0 <cichocjagh@gmail.com>
2024-11-14 13:37:33 +0100

    Implement computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+function getComputerChoice() {
+    const randomInt = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+
+    if (randomInt === 0) {
+        computerChoice = 'rock';
+    } else if (randomInt === 1) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors'
+    }
+
+    return computerChoice;
+}
